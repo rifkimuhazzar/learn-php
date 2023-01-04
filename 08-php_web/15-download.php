7ef461778bb3d5092902472ab64260fa6bb996fe
rifkimuhazzar <rifkiki78@gmail.com>
2023-01-04 16:48:18 +0700

    done

--- 08-php_web/15-download.php
+++ 08-php_web/15-download.php
@@ -2,7 +2,7 @@
 
 if (isset($_GET["key"]) && $_GET["key"] == "rahasia") {
   // echo "Success Key";
-  header("Content-Disposition: attachment; filename='profile.jpg'");
+  header("Content-Disposition: attachment; filename=profile.jpg");
   header("Content-Type: image/jpg"); // bisa tanpa content type juga
   readfile(__DIR__ . "/file/image01.jpg");
   exit();
